a6d387f423379727251718f1414047343a915995
Loren <liangliangpro@outlook.com>
2023-12-20 21:23:24 +0800

    feat: CoreData section 2

--- Bootcamp-CoreData/ContentView.swift
+++ Bootcamp-CoreData/ContentView.swift
@@ -17,40 +17,61 @@ struct ContentView: View {
         sortDescriptors: [NSSortDescriptor(keyPath: \Fruit.name, ascending: false)]
     )
     var fruits: FetchedResults<Fruit>
+    
+    @State var textFieldText: String = ""
 
     var body: some View {
         NavigationView {
-            List {
-                ForEach(fruits) { fruit in
-                    Text(fruit.name ?? "")
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .listStyle(.plain)
-            .navigationTitle("Fruits")
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
+            VStack(spacing: 20) {
+                TextField("Add Fruit here...", text: $textFieldText)
+                    .padding(.leading)
+                    .font(.headline)
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 55)
+                    .background(.gray.opacity(0.3))
+                    .clipShape(RoundedRectangle(cornerRadius: 10.0))
+                    .padding(.horizontal)
+                
+                Button(action: {
+                    addItem()
+                }, label: {
+                    Text("Submit")
+                        .font(.headline)
+                        .foregroundStyle(.white)
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 55)
+                        .background(.blue)
+                        .clipShape(RoundedRectangle(cornerRadius: 10.0))
+                        .padding(.horizontal)
+                })
+                
+                List {
+                    ForEach(fruits) { fruit in
+                        Text(fruit.name ?? "")
+                            .onTapGesture {
+                                updateItem(fruit: fruit)
+                            }
                     }
+                    .onDelete(perform: deleteItems)
                 }
+                .listStyle(.plain)
             }
-            Text("Select an item")
+            .navigationTitle("Fruits")
         }
     }
 
     private func addItem() {
-        // not work
+        // not work on canvas
         withAnimation {
             print("addItem() invoke...")
             
             let newFruit = Fruit(context: viewContext)
-            newFruit.name = "Orange"
+            newFruit.name = textFieldText
 
             saveItems()
+            
+            textFieldText = ""
         }
     }
 
@@ -60,6 +81,17 @@ struct ContentView: View {
             let fruit = fruits[index]
             viewContext.delete(fruit)
 
+            saveItems()
+        }
+    }
+    
+    private func updateItem(fruit: Fruit) {
+        withAnimation {
+            let currentName = fruit.name ?? ""
+            let newName = currentName + "!"
+            
+            fruit.name = newName
+            
             saveItems()
         }
     }
